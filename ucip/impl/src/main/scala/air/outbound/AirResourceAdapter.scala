29699a284bcb945212d008092c2d73074c8aa5ef
ouertani <ouertani@gmail.com>
2012-09-25 18:38:58 +0100

    Annotation

--- ucip/impl/src/main/scala/air/outbound/AirResourceAdapter.scala
+++ ucip/impl/src/main/scala/air/outbound/AirResourceAdapter.scala
@@ -2,20 +2,20 @@ package telcoCx.ucip.spi
 package air.outbound
 
 import javax.resource.ResourceException
-import javax.resource.spi.{ ActivationSpec, BootstrapContext, Connector, ResourceAdapter, ResourceAdapterInternalException, TransactionSupport }
-import javax.resource.spi.endpoint.MessageEndpointFactory;
+import javax.resource.spi._
+import javax.resource.spi.endpoint.MessageEndpointFactory
 import javax.transaction.xa.XAResource
-@Connector(reauthenticationSupport = false,
-  transactionSupport = TransactionSupport.TransactionSupportLevel.NoTransaction,
-  description = Array("JCA 4 UCIP API"),
-  displayName = Array("JCA4UCIP"),
-  vendorName = "TelcoCx",
-  eisType = "UCIP",
-  version = "1.0")
-class AirResourceAdapter extends ResourceAdapter {
+@Connector(
+  reauthenticationSupport = false,
+  transactionSupport = TransactionSupport.TransactionSupportLevel.NoTransaction
+ )
+class AirResourceAdapter() extends ResourceAdapter {
+  var bootstrapContext:BootstrapContext =_
   def getXAResources(ass: Array[ActivationSpec]): Array[XAResource] = null
   def endpointDeactivation(mef: MessageEndpointFactory, as: ActivationSpec) {}
   def endpointActivation(mef: MessageEndpointFactory, as: ActivationSpec) {}
-  def start(bc: BootstrapContext) {}
+  def start(bc: BootstrapContext) {
+    this.bootstrapContext = bc;
+  }
   def stop() {}
 }
